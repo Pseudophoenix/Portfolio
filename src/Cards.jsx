624b48e08594236340b2d7014dd823279f97577d
Alok <waynerooney0089@gmail.com>
2025-04-08 03:28:02 +0530

    Changed

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -4,61 +4,54 @@ import { useState } from 'react';
 import { FaX, FaBars } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 // import Card from './Card.jsx';
-const HomeCard = ({isNavBarClosed,setIsNavBarClosed}) => {
+const HomeCard = ({ isNavBarClosed, setIsNavBarClosed }) => {
   const location = useLocation();
   const isActive = location.pathname === '/' || location.pathname === '/home';
 
   return (
     <div className={`card ${isActive ? 'card-visible' : ''}`}>
-      <div className='card-header' style={{'--clr':' #00ade1'}}>
+      <div className='card-header' style={{ '--clr': ' #00ade1' }}>
         <h2>Intro</h2>
-        <NavLink to="/" className={isNavBarClosed?'cross-button-closed':'cross-button-open'} onClick={()=>setIsNavBarClosed(!isNavBarClosed)}><FaBars/></NavLink>
+        <NavLink to="/" className={isNavBarClosed ? 'cross-button-closed' : 'cross-button-open'} onClick={() => setIsNavBarClosed(!isNavBarClosed)}><FaBars /></NavLink>
       </div>
       {/* <h2>Home</h2> */}
       <div className="card-content">
-        <h3>Alok!</h3>   
-          <ul>
-            <li>Pursing Bachelors From IIIT Manipur</li>
-            <li>Secondary Education Dayawati Modi Public School, Modinagar</li>
-          </ul>
+        <h3>Alok!</h3>
       </div>
     </div>
   );
 };
 
-const AboutCard = ({isNavBarClosed,setIsNavBarClosed}) => {
+const AboutCard = ({ isNavBarClosed, setIsNavBarClosed }) => {
   const location = useLocation();
   const isActive = location.pathname === '/about';
 
   return (
     <div className={`card ${isActive ? 'card-visible' : ''}`} >
-      <div className='card-header' style={{ background: '#ff4443','--clr': '#ff4443' }}>
+      <div className='card-header' style={{ background: '#ff4443', '--clr': '#ff4443' }}>
         <h2>About</h2>
-        <NavLink to="/" className={isNavBarClosed?'cross-button-closed':'cross-button-open'} onClick={()=>setIsNavBarClosed(!isNavBarClosed)}><FaBars/></NavLink>
+        <NavLink to="/" className={isNavBarClosed ? 'cross-button-closed' : 'cross-button-open'} onClick={() => setIsNavBarClosed(!isNavBarClosed)}><FaBars /></NavLink>
       </div>
-      I am 
       <div className='card-body'>
-        I am a 
       </div>
     </div>
   );
 };
 
-const EducationCard = ({isNavBarClosed,setIsNavBarClosed}) => {
+const EducationCard = ({ isNavBarClosed, setIsNavBarClosed }) => {
   const location = useLocation();
   const isActive = location.pathname === '/services';
 
   return (
     <div className={`card ${isActive ? 'card-visible' : ''}`}>
-      <div className='card-header' style={{ background: '#ffdd1c','--clr':'#ffdd1c' }}>
+      <div className='card-header' style={{ background: '#ffdd1c', '--clr': '#ffdd1c' }}>
         <h2>Education</h2>
-        <NavLink to="/" className={isNavBarClosed?'cross-button-closed':'cross-button-open'} onClick={()=>setIsNavBarClosed(!isNavBarClosed)}><FaBars/></NavLink>
+        <NavLink to="/" className={isNavBarClosed ? 'cross-button-closed' : 'cross-button-open'} onClick={() => setIsNavBarClosed(!isNavBarClosed)}><FaBars /></NavLink>
       </div>
       <div className='card-body'>
-        <h2 style={{color:'black'}}>Education</h2>
+        <h2 style={{ color: 'black' }}>Education</h2>
         <div className="card-content">
           <h3></h3>
-
           <p></p>
         </div>
       </div>
@@ -68,22 +61,20 @@ const EducationCard = ({isNavBarClosed,setIsNavBarClosed}) => {
 };
 
 
-const BlogCard = ({isNavBarClosed,setIsNavBarClosed}) => {
+const BlogCard = ({ isNavBarClosed, setIsNavBarClosed }) => {
   const location = useLocation();
   const isActive = location.pathname === '/team';
 
   return (
     <div className={`card ${isActive ? 'card-visible' : ''}`}>
-      <div className='card-header' style={{ background: '#ff0dff','--clr': '#ff0dff' }}>
+      <div className='card-header' style={{ background: '#ff0dff', '--clr': '#ff0dff' }}>
         <h2>Blog</h2>
-        <NavLink to="/" className={isNavBarClosed?'cross-button-closed':'cross-button-open'} onClick={()=>setIsNavBarClosed(!isNavBarClosed)}><FaBars/></NavLink>
+        <NavLink to="/" className={isNavBarClosed ? 'cross-button-closed' : 'cross-button-open'} onClick={() => setIsNavBarClosed(!isNavBarClosed)}><FaBars /></NavLink>
       </div>
       <div className='card-body'>
-        <h2 style={{color:'black'}}>Blog</h2>
+        <h2 style={{ color: 'black' }}>Blog</h2>
         <div className="card-content">
-          <h3>My Projects</h3>
-
-          <p>Discover what we can do for you.</p>
+          <h3></h3>
         </div>
       </div>
 
@@ -92,22 +83,20 @@ const BlogCard = ({isNavBarClosed,setIsNavBarClosed}) => {
 };
 
 
-const ProjectCard = ({isNavBarClosed,setIsNavBarClosed}) => {
+const ProjectCard = ({ isNavBarClosed, setIsNavBarClosed }) => {
   const location = useLocation();
   const isActive = location.pathname === '/team';
 
   return (
     <div className={`card ${isActive ? 'card-visible' : ''}`}>
-      <div className='card-header' style={{ background: '#7bfb00','--clr': '#7bfb00'  }}>
+      <div className='card-header' style={{ background: '#7bfb00', '--clr': '#7bfb00' }}>
         <h2>Project</h2>
-        <NavLink to="/" className={isNavBarClosed?'cross-button-closed':'cross-button-open'} onClick={()=>setIsNavBarClosed(!isNavBarClosed)}><FaBars/></NavLink>
+        <NavLink to="/" className={isNavBarClosed ? 'cross-button-closed' : 'cross-button-open'} onClick={() => setIsNavBarClosed(!isNavBarClosed)}><FaBars /></NavLink>
       </div>
       <div className='card-body'>
         <h2>Project</h2>
         <div className="card-content">
-          <h3>My Projects</h3>
-
-          <p>Discover what we can do for you.</p>
+          <h3></h3>
         </div>
       </div>
 
@@ -115,4 +104,4 @@ const ProjectCard = ({isNavBarClosed,setIsNavBarClosed}) => {
   );
 };
 
-export { HomeCard, AboutCard, EducationCard, ProjectCard,BlogCard };
+export { HomeCard, AboutCard, EducationCard, ProjectCard, BlogCard };
